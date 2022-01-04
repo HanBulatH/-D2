9b0960a2c34f3b11020d37a899eecc3bb0d331c9
Bulat <bulatmiftakhov@mail.ru>
2022-01-04 23:10:49 +0300

    D3 окончательный

--- news/templateage/custom_filters.py
+++ news/templateage/custom_filters.py
@@ -3,10 +3,16 @@ from django import template
 register = template.Library()  # если мы не зарегистрируем наши фильтры, то Django никогда не узнает, где именно их искать и фильтры потеряются.......
 
 @register.filter(name='Censor')
-def Censor (word):
-    word_list = []
-    word_list = word.split()
+def Censor (value):
     cencor_word = ['Россия', 'федерализма', 'МИД']
-    if str(word) in cencor_word:
-        word = 'xxxx'
-        return word
+    word_list = (str(value)).split()
+
+    for i in cencor_word:
+        for j in word_list:
+            if j == i:
+                a = word_list.index(j)
+                word_list.insert(a, '***')
+                word_list.remove(j)
+                value = ' '.join(word_list)
+
+    return value
